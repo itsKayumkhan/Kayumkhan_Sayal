aa659d34f1ebd5e1c1b2f1477d99dfa6302ef5dd
itsKayumkhan <kayumkhankks5@gmail.com>
2024-08-12 18:58:47 +0530

    side nav problem solve

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,9 +1,10 @@
+import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
-import { useEffect } from "react"
+import {  useRef } from "react"
 
 const SideNav = () => {
-
-  useEffect(() => {
+const listRef = useRef(null)
+  useGSAP(() => {
     gsap.from(".list", {
       opacity: 0,
       y: 100,
@@ -16,11 +17,11 @@ const SideNav = () => {
     <>
       <nav className="z-10">
         <ul className='flex flex-col items-center justify-center gap-2 text-3xl'>
-            <li  className='text-3xl flex items-center justify-center icon cursor-pointer   hover:scale-125  w-12 h-12 list z-10'><a target="_blank" href="https://www.linkedin.com/in/kayumkhan-sayal-4aa583264?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><i className="hover:scale-90 duration-300 text-slate-300 hover:text-white ri-linkedin-fill"></i></a></li>
-            <li  className='text-3xl flex items-center justify-center icon cursor-pointer   hover:scale-125  w-12 h-12 list z-10'><a target="_blank" href="https://github.com/itsKayumkhan"><i className="hover:scale-90 duration-300 text-slate-300 hover:text-white ri-github-fill"></i></a></li>
-            <li  className='text-3xl flex items-center justify-center icon cursor-pointer   hover:scale-125  w-12 h-12 list z-10'><a target="_blank" href="https://www.twitter.com/kayumkhankks"><i className="hover:scale-90 duration-300 text-slate-300 hover:text-white ri-twitter-fill"></i></a></li>
-            <li  className='text-3xl flex items-center justify-center icon cursor-pointer   hover:scale-125  w-12 h-12 list z-10'><a target="_blank" href="https://www.instagram.com/codemanoranjan"><i className="hover:scale-90 duration-300 text-slate-300 hover:text-white ri-instagram-line"></i></a></li>
-            <li  className='text-3xl flex items-center justify-center icon cursor-pointer   hover:scale-125  w-12 h-12 list z-10'><a target="_blank" href="https://www.facebook.com/kayumkhankks"><i className="hover:scale-90 duration-300 text-slate-300 hover:text-white ri-facebook-fill"></i></a></li>
+            <li ref={listRef} className='text-3xl flex items-center justify-center icon cursor-pointer   hover:scale-125  w-12 h-12 list z-10'><a target="_blank" href="https://www.linkedin.com/in/kayumkhan-sayal-4aa583264?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><i className="hover:scale-90 duration-300 text-slate-300 hover:text-white ri-linkedin-fill"></i></a></li>
+            <li ref={listRef} className='text-3xl flex items-center justify-center icon cursor-pointer   hover:scale-125  w-12 h-12 list z-10'><a target="_blank" href="https://github.com/itsKayumkhan"><i className="hover:scale-90 duration-300 text-slate-300 hover:text-white ri-github-fill"></i></a></li>
+            <li ref={listRef} className='text-3xl flex items-center justify-center icon cursor-pointer   hover:scale-125  w-12 h-12 list z-10'><a target="_blank" href="https://www.twitter.com/kayumkhankks"><i className="hover:scale-90 duration-300 text-slate-300 hover:text-white ri-twitter-fill"></i></a></li>
+            <li ref={listRef} className='text-3xl flex items-center justify-center icon cursor-pointer   hover:scale-125  w-12 h-12 list z-10'><a target="_blank" href="https://www.instagram.com/codemanoranjan"><i className="hover:scale-90 duration-300 text-slate-300 hover:text-white ri-instagram-line"></i></a></li>
+            <li ref={listRef} className='text-3xl flex items-center justify-center icon cursor-pointer   hover:scale-125  w-12 h-12 list z-10'><a target="_blank" href="https://www.facebook.com/kayumkhankks"><i className="hover:scale-90 duration-300 text-slate-300 hover:text-white ri-facebook-fill"></i></a></li>
         </ul>
       </nav>
     </>
